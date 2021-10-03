63d60590e9a441170191b7d24d0818cd5fde356c
Anirudh Achal <anirudh.achal@gmail.com>
2021-10-03 14:17:17 +0530

    Q1 Completed

---
+++ Week 2/Q1/Client/client.py
@@ -0,0 +1,71 @@
+import socket
+import json
+
+# Constants
+HEADER = 64
+PORT = 8080
+SERVER = '192.168.1.225'
+BASE_URL = 'http://192.168.1.225'
+SERVER_ADDRESS = (SERVER, PORT)
+FORMAT = 'utf-8'
+SOCKET_FAMILY = socket.AF_INET
+SOCKET_PROTOCOL = socket.SOCK_STREAM
+
+
+class Client:
+    def __init__(self, socket_family, socket_protocol, server_address):
+        self.socket_family = socket_family
+        self.socket_protocol = socket_protocol
+        self.server_address = server_address
+        self.client = socket.socket(self.socket_family, self.socket_protocol)
+
+    def request(self, url):
+        self.start()
+
+        try:
+            self.client.connect(self.server_address)
+        except Exception as e:
+            print(e, 'Server not available')
+            return
+
+        print('here')
+        page = url[len(BASE_URL):]
+        request = {
+            'header': {
+                'request-line': f'GET {page} HTTP/1.1',
+                'host': SERVER,
+                'connection': 'close',
+            },
+        }
+
+        request = json.dumps(request, indent=4)
+        self.client.send(request.encode(FORMAT))
+
+        response = self.client.recv(2048).decode(FORMAT)
+        response = json.loads(response)
+
+        print('[SERVER RESPONSE]')
+        print((json.dumps(response, indent=4)), '\n')
+
+        print('[HTML]')
+        print(response['body'], '\n')
+
+        self.stop()
+
+    def start(self):
+        self.client = socket.socket(self.socket_family, self.socket_protocol)
+
+    def stop(self):
+        self.client.close()
+
+
+def main():
+    client = Client(SOCKET_FAMILY, SOCKET_PROTOCOL, SERVER_ADDRESS)
+
+    while True:
+        url = input(f'URL: http://{SERVER}/')
+        url = f'http://{SERVER}/{url}'
+        client.request(url)
+
+
+main()
