37e434a3bf65902ef58feeb92736c54f721a4a38
Anirudh Achal <anirudh.achal@gmail.com>
2021-10-10 11:08:39 +0530

    Week 4 Q1 completed

---
+++ Week 4/Q1/main.py
@@ -0,0 +1,40 @@
+import socket
+
+SOCKET_FAMILY = socket.AF_INET
+SOCKET_PROTOCOL = socket.SOCK_STREAM
+
+
+class PortScanner:
+    @staticmethod
+    def scan(server, start=1, end=65535):
+        print('\nScanning...\n')
+        open_port_count = 0
+        for port in range(start, end + 1):
+            if PortScanner.__connect(server, port):
+                print(f'Port {port} is open')
+                open_port_count += 1
+
+        if open_port_count == 0:
+            print('No ports are available')
+        else:
+            print(f'\nFound {open_port_count} open port{"s" if open_port_count > 1 else ""}\n')
+
+    @staticmethod
+    def __connect(server, port):
+        client = socket.socket(SOCKET_FAMILY, SOCKET_PROTOCOL)
+        address = (server, port)
+        try:
+            client.connect(address)
+            client.close()
+            return True
+        except:
+            return False
+
+
+def main():
+    server = input('Server: ')
+    port_scanner = PortScanner()
+    port_scanner.scan(server, 49660, 49680)
+
+
+main()
